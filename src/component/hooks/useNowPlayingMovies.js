aa4a48b2cb610671ce48670a1b2cfb03679e1ebb
abhijithani <abhijithmscr7@gmail.com>
2025-08-15 00:42:19 +0530

    added try catch

--- src/component/hooks/useNowPlayingMovies.js
+++ src/component/hooks/useNowPlayingMovies.js
@@ -5,20 +5,24 @@ import { addNowPlayingMovies } from "../../utilis/movieSlice";
 import { useSelector } from "react-redux";
 const useNowPlayingMovies = () => {
     //fetch data form TMDB API and updated store
-    const nowPlaying =  useSelector(store => store.movies.addNowPlayingMovies)
+    const nowPlaying = useSelector(store => store.movies.addNowPlayingMovies)
     const dispatch = useDispatch();
 
     const getNowPlaying = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?',
-            API_OPTIONS);
-        const json = await data.json();
-        // console.log(json.results);
-        dispatch(addNowPlayingMovies(json.results))
+        try {
+            const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?',
+                API_OPTIONS);
+            const json = await data.json();
+            // console.log(json.results);
+            dispatch(addNowPlayingMovies(json.results));
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     useEffect(() => {
-        !nowPlaying && getNowPlaying(); 
-    },[]);
+        !nowPlaying && getNowPlaying();
+    }, []);
 };
 
 export default useNowPlayingMovies;
--- src/component/hooks/usePopularMovies.js
+++ src/component/hooks/usePopularMovies.js
@@ -3,22 +3,27 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addPopularMovies } from "../../utilis/movieSlice";
 import { useSelector } from "react-redux";
+
 const usePopularMovies = () => {
     //fetch data form TMDB API and updated store
-    const popular =  useSelector(store => store.movies.addNowPlayingMovies)
+    const popular = useSelector(store => store.movies.addNowPlayingMovies)
     const dispatch = useDispatch();
 
     const PopularMovies = async () => {
-        const data = await fetch('https://api.themoviedb.org/3/movie/upcoming?',
-            API_OPTIONS);
-        const json = await data.json();
-        // console.log(json.results);
-        dispatch(addPopularMovies(json.results))
+        try {
+            const data = await fetch('https://api.themoviedb.org/3/movie/upcoming?',
+                API_OPTIONS);
+            const json = await data.json();
+            // console.log(json.results);
+            dispatch(addPopularMovies(json.results));
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     useEffect(() => {
         !popular && PopularMovies();
-    },[]);
+    }, []);
 };
 
 export default usePopularMovies;
